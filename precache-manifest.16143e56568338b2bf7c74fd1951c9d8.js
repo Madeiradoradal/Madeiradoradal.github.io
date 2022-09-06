6eb09c896f9f3b5691dcf1730afaf7e001bc75a2
Andres Tabares Velez <atabares@visionamos.com>
2022-09-06 12:24:32 -0500

    Updates

---
+++ precache-manifest.16143e56568338b2bf7c74fd1951c9d8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db7c82bf522f2993c6b1f7273d9f300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cfc4020c0236d2d8eb6",
+    "url": "/static/css/2.ede86a65.chunk.css"
+  },
+  {
+    "revision": "7d1f6d5d9beb55b10826",
+    "url": "/static/css/main.5c59a640.chunk.css"
+  },
+  {
+    "revision": "2cfc4020c0236d2d8eb6",
+    "url": "/static/js/2.109ab36b.chunk.js"
+  },
+  {
+    "revision": "ec15c8cbec563da3ac9eea5cd64d9660",
+    "url": "/static/js/2.109ab36b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1f6d5d9beb55b10826",
+    "url": "/static/js/main.b7e0488e.chunk.js"
+  },
+  {
+    "revision": "9d96126a8f18c0ebbaa0",
+    "url": "/static/js/runtime-main.20a4d22a.js"
+  },
+  {
+    "revision": "c783c37bb06ece70f86418cc47b59e39",
+    "url": "/static/media/DSC_7890-HDR.c783c37b.jpg"
+  },
+  {
+    "revision": "8facb6cb729cfb6fff3ad29e94f2a5fb",
+    "url": "/static/media/DSC_7894-HDR-Editar.8facb6cb.jpg"
+  },
+  {
+    "revision": "fd4cacaa0f75d15e753d340d99cac758",
+    "url": "/static/media/DSC_7914-HDR-Editar.fd4cacaa.jpg"
+  },
+  {
+    "revision": "85c6147eda94985abf348c1189ee3578",
+    "url": "/static/media/DSC_8058-HDR-Editar.85c6147e.jpg"
+  },
+  {
+    "revision": "164a78bc0a0ad4ab7dcfbda74b047d00",
+    "url": "/static/media/DSC_8122-HDR-Editar.164a78bc.jpg"
+  },
+  {
+    "revision": "92392d15b69db883f26fbdebc5015b46",
+    "url": "/static/media/DSC_8128-HDR-Editar.92392d15.jpg"
+  },
+  {
+    "revision": "c6ed81dd7b049ea2040f1300cb78060a",
+    "url": "/static/media/about.c6ed81dd.svg"
+  },
+  {
+    "revision": "e65a6536fd277930d0d7eeccd7dfb89f",
+    "url": "/static/media/customer.e65a6536.svg"
+  },
+  {
+    "revision": "f6f05af4ebfd528b999e303df5a90263",
+    "url": "/static/media/customer1.f6f05af4.svg"
+  },
+  {
+    "revision": "9dac55866191ecfd805f015323ccf807",
+    "url": "/static/media/customer3.9dac5586.svg"
+  },
+  {
+    "revision": "d1e8cb2d89d663e971662c51c0df9963",
+    "url": "/static/media/en.d1e8cb2d.svg"
+  },
+  {
+    "revision": "6edb36f3feccea6f8483d2808828b6e1",
+    "url": "/static/media/error.6edb36f3.jpg"
+  },
+  {
+    "revision": "c193004b3f55b9338bc1b3c9692f4744",
+    "url": "/static/media/es.c193004b.svg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/static/media/notfound.8c3d2a79.svg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/static/media/room-3.9a1103b5.jpeg"
+  }
+]);
